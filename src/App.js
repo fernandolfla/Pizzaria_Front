f3b8b2be4f8e9869f52995fa1f2182fbf248e542
Luiz Fernando de Lima Avelar <fernandolfla@gmail.com>
2023-03-10 13:34:22 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Site build ok V2.
+          Edit <code>src/App.js</code> Site build ok V3.
         </p>
         <a
           className="App-link"
